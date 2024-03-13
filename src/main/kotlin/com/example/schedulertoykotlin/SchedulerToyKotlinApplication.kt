682bede7ea671113dc16afa3a284b86114ac9eec
ACACIAN <gkehdrn95@gmail.com>
2024-03-13 17:03:50 +0900

    init

---
+++ src/main/kotlin/com/example/schedulertoykotlin/SchedulerToyKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.schedulertoykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchedulerToyKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchedulerToyKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/schedulertoykotlin/SchedulerToyKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.schedulertoykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchedulerToyKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
